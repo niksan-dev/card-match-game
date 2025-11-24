7e55a3a433286fb38f995a84156d8025b6435c9a
niksan-dev <san.patil1439@gmail.com>
2025-11-24 13:00:58 +0530

    Card Factory and Pool aded

---
+++ Assets/_CardGame/Scripts/GamePlay/CardFactory.cs
@@ -0,0 +1,45 @@
+// CardFactory.cs
+using UnityEngine;
+using Niksan.CardGame.Data;
+namespace Niksan.CardGame
+{
+
+    [System.Serializable]
+    public class CardFactory : MonoBehaviour
+    {
+        [SerializeField] private BasicCard _cardPrefab;
+        [SerializeField] private Transform _cardParent;
+        [SerializeField] private int _initialPoolSize = 10;
+
+        private ObjectPool<BasicCard> _pool;
+
+
+        void Start()
+        {
+            Initialize();
+        }
+        public void Initialize()
+        {
+            _pool = new ObjectPool<BasicCard>(_cardPrefab, _cardParent, _initialPoolSize);
+        }
+
+        public BasicCard CreateCard(CardData data, Vector3 position)
+        {
+            BasicCard card = _pool.Get();
+            card.transform.position = position;
+            card.transform.localScale = Vector3.one;
+            card.SetData(data);
+            return card;
+        }
+
+        public void RecycleCard(BasicCard card)
+        {
+            _pool.ReturnToPool(card);
+        }
+
+        public void Clear()
+        {
+            _pool.Clear();
+        }
+    }
+}
